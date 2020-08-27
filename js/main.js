6132eb9e2f819ea7419adc570fc8e8a1c4af0f5e
Asmita Gauri <asmitagaurikhan@gmail.com>
2020-08-27 14:24:17 +0530

    Updated Home Page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.venobox').venobox();
+});
+
+// Init AOS
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        once: true
+    });
+}
+$(window).on('load', function () {
+    aos_init();
+});
